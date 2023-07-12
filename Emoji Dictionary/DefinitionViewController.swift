79850c43c0cdaea9d6ba72fbc83fc4eee8d6b2c2
AndresImery <andresimery@gmail.com>
2023-07-12 16:38:08 -0400

    App Done!

---
+++ Emoji Dictionary/DefinitionViewController.swift
@@ -0,0 +1,42 @@
+//
+//  DefinitionViewController.swift
+//  Emoji Dictionary
+//
+//  Created by Andres Imery on 7/12/23.
+//
+
+import UIKit
+
+class DefinitionViewController: UIViewController {
+
+    @IBOutlet weak var definitionLabel: UILabel!
+    
+    @IBOutlet weak var emojiLabel: UILabel!
+    
+    var emoji = "NO EMOJI"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        emojiLabel.text = emoji
+        
+        if emoji == "😎" {
+            definitionLabel.text = "I'm the Coolest Emoji!"
+        } else if emoji == "💩" {
+            definitionLabel.text = "The Stinkiest Emoji! A poop!"
+        } else if emoji == "😀" {
+            definitionLabel.text = "I'm so Happy!"
+        } else if emoji == "🎅🏿" {
+            definitionLabel.text = "The Best Santa!"
+        } else if emoji == "🐹" {
+            definitionLabel.text = "A cute Hamster!"
+        } else if emoji == "🐬" {
+            definitionLabel.text = "A little Dolphin!"
+        } else {
+            definitionLabel.text = "A Lovely Church"
+        }
+    }
+
+}
